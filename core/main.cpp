96458d3a08ca919394694f7ead1153206ae6acaa
akicatt <markpustog@gmail.com>
2026-09-15 00:02:14 +0500

    qt stuff

--- core/main.cpp
+++ core/main.cpp
@@ -8,58 +8,35 @@
 #include <stdexcept>
 #include <string>
 #include <thread>
+
 #include <sodium.h>
 
-#include "local_server.hpp"
+#include <QFontDatabase>
+#include <qqmlcontext.h>
+
 #include "client.hpp"
 #include "coh.hpp"
-#include "identity.hpp"
-
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
-#include <QFontDatabase>
-
-#include "qt_ptrs.hpp"
+#include "qt_connector.hpp"
 
 namespace asio = boost::asio;
 
 namespace mydak {
 	static void input(
 	client& client,
-		asio::io_context& io,
-		const std::shared_ptr<local_server>& local_server
+		asio::io_context& io
 	) {
 		for (;;) {
 			// INPUT
 			std::string input{};
 			std::getline(std::cin, input);
 
-
-			if (input == "/server") {
-				asio::post(io, [local_server, &io]() {
-					local_server->acceptor = std::make_shared<asio::ip::tcp::acceptor>(
-						io,
-						asio::ip::tcp::endpoint(asio::ip::make_address("127.0.0.1"), 6767)
-					);
-					local_server->local_receive();
-				});
-			} else {
-				client.messages.emplace(input);
-
-				coh::detached(
-					[client]() -> asio::awaitable<void>{
-						const boost::system::error_code e;
-						co_await client.send_channel_ptr->async_send(e, asio::use_awaitable);
-						co_return;
-					}
-				);
-			}
+			client.send_message(input);
 		}
 	}
 
-	static int qt(
+	static int qt_initialization(
 		int argc, char* argv[],
-		qt_ptrs& ptrs, std::promise<void>& qt_signal_promise
+		qt_ptrs& qt_pointers, std::promise<void>& qt_signal_promise
 	) {
 		QGuiApplication app(argc, argv);
 
@@ -76,12 +53,14 @@ namespace mydak {
 		app_engine.loadFromModule("mydak_backend", "Main");
 
 		if (app_engine.rootObjects().isEmpty()) return -1;
-		auto main = app_engine.rootObjects().constFirst();
+		const auto main = app_engine.rootObjects().constFirst();
 
 		// QT PTRS
-		ptrs.messages = main->findChild<QObject*>("messages_rectangle");
+		qt_pointers.messages_rectangle = main->findChild<QObject*>("messages_rectangle");
+		qt_pointers.recipient_rectangle = main->findChild<QObject*>("recipient_rectangle");
+		qt_pointers.app = &app;
+		qt_pointers.app_engine = &app_engine;
 		qt_signal_promise.set_value();
-
 
 
 		return QGuiApplication::exec();
@@ -97,7 +76,7 @@ int main(int argc, char* argv[]) {
 	#pragma region QT
 	mydak::qt_ptrs qt_pointers;
 	std::promise<void> qt_signal;
-	std::thread qt_thread(mydak::qt, argc, argv, std::ref(qt_pointers), std::ref(qt_signal));
+	std::thread qt_thread(mydak::qt_initialization, argc, argv, std::ref(qt_pointers), std::ref(qt_signal));
 	qt_signal.get_future().get(); // Wait for qt_thread to get all the refs
 
 	qt_thread.detach();
@@ -106,19 +85,20 @@ int main(int argc, char* argv[]) {
 	auto& io = mydak::coh::io();
 	mydak::client client(io, "127.0.0.1", "8888", qt_pointers, argc, argv);
 
+	// Qt late init
+	mydak::qt_connector connector(client);
+	qt_pointers.app_engine->rootContext()->setContextProperty("qt_connector", &connector);
+
 	mydak::coh::detached(client.initialize(0));
 
 	io.run();
 	io.restart();
 
-	mydak::coh::detached(client.receive());
-	mydak::coh::detached(client.send());
+	mydak::coh::detached(client.receive_loop());
+	mydak::coh::detached(client.send_loop());
 
 
-	// INPUT
-	auto local_server = std::make_shared<mydak::local_server>(io, client);
-
-	std::thread input_thread(mydak::input, std::ref(client), std::ref(io), local_server);
+	std::thread input_thread(mydak::input, std::ref(client), std::ref(io));
 	input_thread.detach();
 
 	io.run();
